8151a0bc99c646bfd94e1639e7315d32edd5b1df
Lincoln Moura <lincoln182lhp@gmail.com>
2021-10-21 02:07:26 -0300

    ajustes

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 let express = require('express');
-app = express();
-const cors = require('cors');
-const port = 3000;
-five = require("johnny-five");
-board = new five.Board();
-led = null;
-diodo = null;
-thermometer = null;
-
-
-
-const { json, urlencoded } = require('body-parser');
 const { response } = require('express');
+const cors = require('cors');
+const { json, urlencoded } = require('body-parser');
+five = require("johnny-five");
 
+const port = 3000;
+board = new five.Board();
+
+let ledSala = null,
+    ledQuarto = null,
+    ledCozinha = null,
+    diodo = null,
+    thermometer = null;
+
+app = express();
 app.use(cors())
 app.use(json());
 app.use(urlencoded({ extended: true }));
@@ -21,38 +22,55 @@ app.get('/', (req, res) => {
     res.status(200).json({ message: 'Hello App' });
 });
 
-
 board.on("ready", function () {
-    console.log("Conectado");
-    led = new five.Led(11);
-    diodo = new five.Led(9);
+    console.log("Conexão com a placa concluída");
 
-    /*
+    ledSala = new five.Led(13);
+    ledQuarto = new five.Led(12);
+    ledCozinha = new five.Led(11);
+    diodo = new five.Led(10);
+
     thermometer = new five.Thermometer({
         controller: "LM35",
-        pin: "A0"
+        pin: "A1"
     });
 
+    /*
     thermometer.on("change", () => {
-        const { celsius, fahrenheit, kelvin } = thermometer;
+        const {celsius} = thermometer;
         console.log("Thermometer");
         console.log("  celsius      : ", celsius);
-        console.log("  fahrenheit   : ", fahrenheit);
-        console.log("  kelvin       : ", kelvin);
-        console.log("--------------------------------------");
-    });*/
+      });
+
+     
+    photoresistor = new five.Sensor({
+        pin: "A2",
+        freq: 250
+    });
+
+    //repl permite acesso direto ao sensor
+    board.repl.inject({
+        pot: photoresistor
+    });
+
+    
+    photoresistor.on("data", function() {
+        console.log(this.value);
+    });
+    */
 });
 
 app.post('/led/:mode', function (req, res) {
-    console.log(req.params)
-    if (led) {
-        var status = "OK";
+    console.log(req.params);
+    
+    if (ledSala){
+        var status = "Sala OK";
         switch (req.params.mode) {
             case "on":
-                led.on();
+                ledSala.on();
                 break;
             case "off":
-                led.off();
+                ledSala.off();
                 break;
             default:
                 status = "Comando não encontrado: " + req.params.mode;
@@ -60,13 +78,49 @@ app.post('/led/:mode', function (req, res) {
         }
         console.log(status);
         res.send(status);
-    } else {
+    }
+
+    else if (ledQuarto) {
+        var status = "Quarto OK";
+        switch (req.params.mode) {
+            case "on":
+                ledQuarto.on();
+                break;
+            case "off":
+                ledQuarto.off();
+                break;
+            default:
+                status = "Comando não encontrado: " + req.params.mode;
+                break;
+        }
+        console.log(status);
+        res.send(status);
+    }
+
+    else if (ledCozinha) {
+        var status = "Cozinha OK";
+        switch (req.params.mode) {
+            case "on":
+                ledCozinha.on();
+                break;
+            case "off":
+                ledCozinha.off();
+                break;
+            default:
+                status = "Comando não encontrado: " + req.params.mode;
+                break;
+        }
+        console.log(status);
+        res.send(status);
+    }
+    
+    else {
         res.send('Placa não conectada')
     }
 });
 
-app.get('/diodo/:mode', function (req, res) {
-
+app.post('/diodo/:mode', function (req, res) {
+    console.log(req.params)
     if (diodo) {
         var status = "OK";
         switch (req.params.mode) {
@@ -88,7 +142,10 @@ app.get('/diodo/:mode', function (req, res) {
 });
 
 app.get('/temperatura', (req, res) => {
-    res.status(200).json({ temperatura: Math.random() })
+
+    res.status(200).json({
+        temperatura: Math.random()
+    });
 });
 
 app.listen(port, function () {
--- index.js
+++ index.js
@@ -1,40 +1,92 @@
 const baseUrl = 'http://localhost:3000';
 
-const addEventCheckbox = () => {
-    document.querySelector("#botaoLuz").addEventListener('click', () => {
-        if (isChecked()) {
-            setOn();
+const eventoCheckbox = () => {
+
+    document.querySelector("#luzSala").addEventListener('click', () => {
+        if (checarLuzSala()) {
+            ligarLuz();
         }
         else {
-            setOff();
+            desligarLuz();
+        }
+    });
+
+    document.querySelector("#luzQuarto").addEventListener('click', () => {
+        if (checarLuzQuarto()) {
+            ligarLuz();
+        }
+        else {
+            desligarLuz();
+        }
+    });
+
+    document.querySelector("#luzCozinha").addEventListener('click', () => {
+        if (checarLuzCozinha()) {
+            ligarLuz();
+        }
+        else {
+            desligarLuz();
+        }
+    });
+
+    document.querySelector("#botaoAlarme").addEventListener('click', () => {
+        if (isCheckedAlarm()) {
+            ligarAlarme();
+        }
+        else {
+            desligarAlarme();
         }
     });
 }
 
 const adicionarEventoTemperatura = () => {
        setInterval(async () => {
-        document.querySelector('#botaoTemperaturar').value = await obterTemperatura();
+        document.querySelector('#botaoTemperatura').value = await obterTemperatura();
         }, 1000);
 }
 
-const isChecked = () => {
-    return document.querySelector('#botaoLuz').checked;
+// LUZ
+const checarLuzSala = () => {
+    return document.querySelector('#luzSala').checked;
 }
 
-const setOn = () => {
+const checarLuzQuarto = () => {
+    return document.querySelector('#luzQuarto').checked;
+}
+
+const checarLuzCozinha = () => {
+    return document.querySelector('#luzCozinha').checked;
+}
+
+const ligarLuz = () => {
     axios.post(`${baseUrl}/led/on`);
 }
 
-const setOff = () => {
+const desligarLuz = () => {
     axios.post(`${baseUrl}/led/off`);
 }
 
+//ALARME
+const isCheckedAlarm = () => {
+    return document.querySelector('#botaoAlarme').checked;
+}
+
+const ligarAlarme = () => {
+    axios.post(`${baseUrl}/diodo/on`);
+}
+
+const desligarAlarme = () => {
+    axios.post(`${baseUrl}/diodo/off`);
+}
+
+// TEMPERATURA
 const obterTemperatura = async () => {
     return (await axios.get(`${baseUrl}/temperatura`)).data.temperatura
 }
 
+// ADICIONAR EVENTOS
 const addEvents = () => {
-    addEventCheckbox();
+    eventoCheckbox();
     adicionarEventoTemperatura();
 }
 
